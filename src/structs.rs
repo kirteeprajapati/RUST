f8f78d5352d8904e269644b2bc768bff9a70c119
Kirtee Prajapati <95649744+kirteeprajapati@users.noreply.github.com>
2022-05-10 02:11:39 +0530

    Update structs.rs

--- src/structs.rs
+++ src/structs.rs
@@ -15,13 +15,27 @@ struct Person{
     last_name: String
 }
 
-impl Person{
+impl Person{                            //impl = impliment
     // Construct Person
     fn new(first: &str, last:&str)->Person{
         Person{
             first_name: first.to_string(),
             last_name: last.to_string()
         }
+    }
+    // Get full name
+    fn full_name(&self)->String{
+        format!("{} {}", self.first_name, self.last_name)
+    }
+        
+    // Set last name
+    fn set_last_name(&mut self, last: &str){
+        self.last_name = last.to_string();
+    }
+        
+    // Name to tuple
+    fn to_tuple(self)->(String, String){
+        (self.first_name, self.last_name)
     }
 }
 
@@ -40,4 +54,14 @@ pub fun structs(){
     
     c.0 = 200;
     println!("Color: {} {} {}", c_tup.0, c_tup.1, c_tup.2);
+        
+    let mut p = Person::new("Kirtee", "Prajapati");
+    println!("Person {} {}", p.first_name, p.last_name);
+        
+    println!("Person {}", p.full_name());
+        
+    p.set_last_name("Kumari");
+    println!("Person {}", p.full_name());    
+        
+    println!("Person Tuple {:?}", p.to_tuple());
 }
