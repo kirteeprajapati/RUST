3a4ca23f62810c5fab0b28e948f96d46f6eb8dc8
Kirtee Prajapati <95649744+kirteeprajapati@users.noreply.github.com>
2022-05-10 02:22:27 +0530

    Create enums.rs

---
+++ src/enums.rs
@@ -0,0 +1,20 @@
+enums Movement{
+    // Variable
+    Up,
+    Down,
+    Left,
+    Right
+}
+
+fn move_avatar(s: Movement){
+    //perform action depending an info
+    match = {
+        Movements1::Up=> print("vatar moving up"),
+        Movements2::Down=> print("vatar moving up"),
+        Movements3::Left=> print("vatar moving up").Movements::Up=> print("vatar moving up")Movements::Up=> print("vatar moving up")Movements::Up=> print("vatar moving up")Movements::Up=> print("vatar moving up")Movements::Up=> print("vatar moving up")Movements::Up=> print("vatar moving up")Movements::Up=> print("vatar moving up")Movements::Up=> print("vatar moving up")
+        
+    }
+}
+pub fn enums(){
+
+}
